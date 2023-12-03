d38a98d3cd7c4a793a46daa7bc3bde0161dd9b6d
RetroZelda <efolckemer@retrozelda.com>
2023-12-03 00:33:24 -0500

    its ok to not have a log

--- scripts/server.py
+++ scripts/server.py
@@ -20,14 +20,18 @@ socketio = SocketIO(app)
 def read_log():
     last_position = 0
     while True:
-        with open(args.log_to_read, 'r') as log_file:
-            log_file.seek(last_position)
-            new_lines = [line.strip() for line in log_file.readlines() if line.strip()]
+        try:
+            with open(args.log_to_read, 'r') as log_file:
+                log_file.seek(last_position)
+                new_lines = [line.strip() for line in log_file.readlines() if line.strip()]
 
-            if new_lines:
-                socketio.emit('update_log', {'new_lines': new_lines})
+                if new_lines:
+                    socketio.emit('update_log', {'new_lines': new_lines})
 
-            last_position = log_file.tell()  # Update the last position to the current position in the file
+                last_position = log_file.tell()  # Update the last position to the current position in the file
+        except FileNotFoundError:
+            print(f"Log File {args.log_to_read} not found.")
+            break
 
 @app.route('/')
 def index():
