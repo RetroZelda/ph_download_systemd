89b61e6a252180abef215f02f930d755b3d2e5b4
RetroZelda <efolckemer@retrozelda.com>
2024-07-19 02:17:42 -0400

    move completed to a done folder

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,11 +5,16 @@ command -v inotifywait >/dev/null 2>&1 || { echo >&2 "Please install inotify-too
 
 folder_to_monitor=$(readlink -f "$(pwd)/../input")
 folder_config=$(readlink -f "$(pwd)/../config")
+folder_done=$(readlink -f "$(pwd)/../done")
 
 # Create the folder if it doesn't exist
 if [ ! -d "$folder_to_monitor" ]; then
     echo "Creating folder: $folder_to_monitor"
     mkdir -p "$folder_to_monitor"
+fi
+if [ ! -d "$folder_done" ]; then
+    echo "Creating folder: $folder_done"
+    mkdir -p "$folder_done"
 fi
 
 echo "Add a file with a unique URL per line inside of: $folder_to_monitor"
@@ -22,7 +27,7 @@ inotifywait -m -e create "$folder_to_monitor" |
 
                 python3 ./download.py -i "$existing_file" -c "$folder_config"
                 echo "$existing_file has finished processing"
-                rm "$existing_file"
+                mv "$existing_file" "$folder_done"
             fi
         done
     done
